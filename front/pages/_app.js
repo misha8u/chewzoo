4422f10cd7b7ede75bd53075e6e27b2bb9b800f0
misha <rong23@naver.com>
2021-08-10 00:39:41 +0900

    alpha 1.2.2

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -9,11 +9,11 @@ import wrapper from '../store/configureStore';
 const cHEWzOO = ({ Component }) => (
   <>
     <Head>
-      <title>츄주 | 요즘 갬성 주식</title>
+      <title>츄주 | 요즘 느낌의 주식</title>
       {process.env.NODE_ENV === 'production' && <script data-ad-client="ca-pub-9319397510907127" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>}
     </Head>
     <Component />
-    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="mobile-web-app-capable" content="yes" />
   </>
 );
 
